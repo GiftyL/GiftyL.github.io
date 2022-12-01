2e04a12fba4b8ddd10dbc3ad6d776d975d9db99f
GiftyL <99997009+GiftyL@users.noreply.github.com>
2022-12-01 15:11:40 +0300

    Update script.js

--- script.js
+++ script.js
@@ -91,7 +91,7 @@ let geocode = {
       geocode.reverseGeocode(data.coords.latitude, data.coords.longitude);
     }
     if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition(success,'console.error );
+      navigator.geolocation.getCurrentPosition(success, console.error);
     }
     else {
       weather.fetchWeather("Adama");
